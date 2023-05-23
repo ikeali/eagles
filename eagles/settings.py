f59a86291b570e5ee55ac5afd5fed119a346bb08
ikeali <ikechukwualisigwe01@gmail.com>
2023-05-23 10:52:44 +0100

    seconnd commit

--- eagles/settings.py
+++ eagles/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0#tfet=!r!-7t6m@@*va_c)98gko=#hxwqh-4reh9^9b0^u@p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.49.240.147']
 
 
 # Application definition
@@ -121,7 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT =os.path.join (BASE_DIR,'static')
-MEDIA_ROOT = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT =os.path.join (BASE_DIR,'media')
 
 # Default primary key field type
