438aceb4543110946e698be14d93388c534803be
ikeali <ikechukwualisigwe01@gmail.com>
2024-09-02 17:29:49 +0100

    Update settings.py

--- eagles/settings.py
+++ eagles/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-0#tfet=!r!-7t6m@@*va_c)98gko=#hxwqh-4reh9^9b0^u@p4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.48.249.106']
+# ALLOWED_HOSTS = ['13.48.249.106']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
